7df11d6add5a35cae4c91c003857c2ab97abb558
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2013-02-13 12:16:29 +0100

    dodano common

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include <unistd.h>
+
+#include <common.h>
+
+// serwer
+int main(int argc, char* argv[]) {
+  sprobuj_utworzyc_repo();
+
+  podlacz_sie_do_repo();
+
+
+
+  return EXIT_SUCCESS;
+}
